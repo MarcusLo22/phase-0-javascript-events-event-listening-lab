2c44e31ec1709d89639d2f206cce26a276423e47
Marcus Lo <marcusklo1010@gmail.com>
2024-03-17 20:39:35 -0600

    javascript events listening lab complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
-}
+// Get the element you want to add the event listener to
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+  }
+  
+  document.addEventListener('DOMContentLoaded', addingEventListener);
